87794bd457ba79bc8d28f34e2ab58cb61b165cd5
Jay Bulsara <jaybulsara@proton.me>
2022-05-12 11:40:59 -0400

    Added git config commands.

--- git-setup.sh
+++ git-setup.sh
@@ -1,4 +1,10 @@
-# Installs the git prompt
+# Configures git and installs the git prompt
+
+# Configure git
+git config --global user.email jaybulsara@proton.me
+git config --global user.name "Jay Bulsara"
+git config --global credential.helper store
+git config --global init.defaultBranch main
 
 # Download git-prompt.sh
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
